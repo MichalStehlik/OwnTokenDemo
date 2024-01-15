6f8fa414a3f8071fead72139813ab613e1b0b1d9
Michal Stehlík <michal.stehlik@pslib.cz>
2024-01-15 10:38:06 +0100

    auth finished

---
+++ Authority/Models/LoginIM.cs
@@ -0,0 +1,8 @@
+﻿namespace Authority.Models
+{
+    public class LoginIM
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
